a64171c2ab567944adda1a26342ca80d937773c5
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-04-16 20:51:07 +0530

    form added

--- react_city/src/components/Homepage.jsx
+++ react_city/src/components/Homepage.jsx
@@ -1,12 +1,43 @@
 import React from "react";
-import { ResponsiveAppBar } from "./NavBar";
+import react, { useEffect, useState } from "react";
+import axios from "axios";
+import { getAccordionDetailsUtilityClass } from "@mui/material";
 
-const Country = () => {
+export default function Homepage() {
+  const [city, setCity] = useState([]);
+
+  useEffect(() => {
+    getAccordionDetailsUtilityClass();
+  }, []);
+
+  const getData = async (data) => {
+    const res = await axios.get("http://localhost:3001/cities");
+    setCity(res.data[0]);
+  };
+
   return (
-    <>
-      <h1>Add Country</h1>
-    </>
+    <div>
+      <h1>Home</h1>
+      <table border="1px solid black">
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>Country</td>
+            <td>City</td>
+            <td>Population</td>
+            <td>Edit</td>
+            <td>Delete</td>
+          </tr>
+        </thead>
+        <tbody>
+          <td>{city.id}</td>
+          <td>{city.country}</td>
+          <td>{city.city_name}</td>
+          <td>{city.population}</td>
+          <td>Edit</td>
+          <td>Delete</td>
+        </tbody>
+      </table>
+    </div>
   );
-};
-
-export default Country;
+}
